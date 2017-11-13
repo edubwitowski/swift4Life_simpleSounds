da2d2c7051071846bc484604c51703334708f084
edubwitowski <31215451+edubwitowski@users.noreply.github.com>
2017-11-13 10:29:36 -0600

    final Assignment_Simple_Sounds

--- Simple Center/ViewController.swift
+++ Simple Center/ViewController.swift
@@ -9,7 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    ///Mark: Variables for Sound
+    let catSound = SimpleSound(named: "meow")
+    let dogSound = SimpleSound(named: "woof")
+    let rhinoSound = SimpleSound(named: "moo")
+    
+    //Mark: Label - (animalSound)
+    @IBOutlet weak var animalSound: UILabel!
+    
+    
+    //Mark: catButton()OutLet/Action
+    @IBOutlet weak var catButtonMeow: UIButton!
+    @IBAction func catButtonAction(_ sender: Any) {
+        animalSound.text = "Meow!"
+        catSound.play()
+    }
+    
+    //Mark: dogButton()Outlet/Action
+    @IBOutlet weak var dogButtonOutlet: UIButton!
+    @IBAction func dogButtonAction(_ sender: Any) {
+        animalSound.text = "woof!"
+        dogSound.play()
+    }
+    
+    //Mark: rhinoButton()Outlet/Action
+    @IBOutlet weak var rhinoButtonOutlet: UIButton!
+    @IBAction func rhinoButtonAction(_ sender: Any) {
+        animalSound.text = "Charge!"
+        rhinoSound.play()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
